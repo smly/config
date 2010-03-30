da163c98a7e13d8b59c639401577fa59cd09af6b
smly <eowner@gmail.com>
2010-03-31 06:16:20 +0900

    add LB_LIBRARY_PATH

--- .zsh/local/sage.zsh
+++ .zsh/local/sage.zsh
@@ -3,4 +3,5 @@ export GOARCH=amd64
 export GOOS=linux
 export GOBIN=$HOME/local/bin
 
-export PATH=$PATH:$HOME/local/bin
+export PATH=$HOME/local/bin:/opt/bin32-jdk/bin:/usr/bin/perlbin/core:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/local/lib
