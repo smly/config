8925c774b4a3df4f8d81dc21f0bdf43f2d7c041a
smly <eowner@gmail.com>
2015-01-14 21:20:20 +0900

    Fix typo in setup script, setup_tmuxpowerline.sh

--- setup_tmuxpowerline.sh
+++ setup_tmuxpowerline.sh
@@ -2,7 +2,7 @@
 
 cd $HOME/gitws/config
 mkdir -p trunk
-git clone https://gitub.com/erikw/tmux-powerline.git trunk/tmux-powerline
+git clone https://github.com/erikw/tmux-powerline.git trunk/tmux-powerline
 rm $HOME/gitws/config/trunk/tmux-powerline/themes/default.sh
 ln -s $HOME/gitws/config/tmux-theme/default.sh $HOME/gitws/config/trunk/tmux-powerline/themes/default.sh
 ln -s $HOME/gitws/config/.tmux.conf $HOME/.tmux.conf
